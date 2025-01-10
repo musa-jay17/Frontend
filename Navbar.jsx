9c5db123e3e38d2dffc58f1c66f2a5773dec1da7
Musa Jalloh <Musajay17@gmail.com>
2025-01-10 23:11:40 +0000

    Component/Navbar

---
+++ Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+import logo from "../../assets/logo.png"
+
+const Navbar = () => {
+  return (
+    <nav className='container'>
+      <img src={logo} alt=""className='logo' />
+      <ul>
+        <li>Home</li>
+        <li>Donation</li>
+        <li>Volunteer</li>
+        <li>About</li>
+        <li><button className='btn'>Contact us</button></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
