f682b65e861ef2f3cbfc36c8e7916d9a8b9de89c
Musa Jalloh <Musajay17@gmail.com>
2025-01-10 23:10:03 +0000

    Component/black
    background description

---
+++ Black.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Black.css'
+
+const Black = () => {
+  return (
+    <div className='Black container'>
+      <div className='Black-text'>
+        <h1>AL MASAAJID</h1>
+        <p>"In houses which Allah has permitted to be raised and His Name to be mentioned therein. Exalting Him therein in the morning and the evenings."
+        (Surah An-Nur, 24:36)</p>
+        <button className='btn'>Explore more</button>
+        </div>
+    </div>
+  )
+}
+
+export default Black
